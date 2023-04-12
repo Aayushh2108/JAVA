41578b704f237ac05b56e155b86edf8e65c19122
Ayush <119883228+Aayushh2108@users.noreply.github.com>
2023-04-12 14:40:21 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,16 @@
+
+public class Student {
+	
+		static int num1= 21;
+		public static void fun() {
+			num1++;
+			System.out.println(num1);
+			
+		}
+		public static void main(String[] args) {
+			fun();
+			fun();
+			fun();
+	}
+
+}
