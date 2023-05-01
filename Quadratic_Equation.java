8de9e76e052e247636bd2332edf5d931b3272cbb
Ayush <119883228+Aayushh2108@users.noreply.github.com>
2023-05-01 23:27:54 +0530

    Add files via upload

---
+++ Quadratic_Equation.java
@@ -0,0 +1,14 @@
+
+public class Quadratic_Equation {
+
+	public static void main(String[] args) {
+//		Quadratic Equation//
+		
+		int a=10;
+		int b =4;
+		int c= 5;
+		int k= b*b - (4*a*c)/(2*a);
+		System.out.println(k);
+	}
+
+}
